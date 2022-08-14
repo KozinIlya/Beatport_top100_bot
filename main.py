e55a29fce0706ba5b06b38cde4838634147414ee
ilakozin <i.a.kozin@gmail.com>
2022-08-14 19:28:50 +0300

    fin. file

--- main.py
+++ main.py
@@ -5,62 +5,75 @@ import requests
 from bs4 import BeautifulSoup
 import settings
 import os
-
-url = 'https://www.beatport.com/top-100-releases'
-request = requests.get(url)
-soup = BeautifulSoup(request.text, 'html.parser')
-
-
-def fun(s):
-    t = s.find_all('li', class_="bucket-item ec-item horz-release")
-    title_top100 = []
-    position_top100 = []
-    lable_top100 = []
-    link_top100 = []
-
-    for i in t:
-        title = i.find('p', class_='buk-horz-release-title').text
-        position = i.find('p', class_="buk-horz-release-num").text
-        lable = i.find('p', class_="buk-horz-release-labels").text
-        link = i.find('a', href=True)['href']
-
-        title_top100.append(title)
-        position_top100.append(position)
-        lable_top100.append(lable)
-        link_top100.append(link)
-    s = {position: position_top100, title: title_top100, lable: lable_top100, link: link_top100}
-    df = pd.DataFrame(s)
-    df.columns = ['position', 'title', 'lable', 'link']
-    df['lable'] = df['lable'].str.replace("\n", "")
-    df.set_index('position')
-    return df
+import io
+import logging
+dir_pass = '/Users/ilakozin/PycharmProjects/Beatport_top100_bot/cronos_logs'
+def cronos(chat=None):
+    url = 'https://www.beatport.com/top-100-releases'
+    request = requests.get(url)
+    soup = BeautifulSoup(request.text, 'html.parser')
 
 
-while request.status_code != 200:
-    time.sleep(60)
-    print('sleep')
-df = fun(soup)
+    def fun(s):
+        t = s.find_all('li', class_="bucket-item ec-item horz-release")
+        title_top100 = []
+        position_top100 = []
+        lable_top100 = []
+        link_top100 = []
 
-bot = telegram.Bot(token=os.getenv('TOKEN'))
+        for i in t:
+            title = i.find('p', class_='buk-horz-release-title').text
+            position = i.find('p', class_="buk-horz-release-num").text
+            lable = i.find('p', class_="buk-horz-release-labels").text
+            link = i.find('a', href=True)['href']
 
-df_old = pd.read_csv('/Users/ilakozin/PycharmProjects/Beatport_top100_bot/Beatport_top100_releases.csv')
+            title_top100.append(title)
+            position_top100.append(position)
+            lable_top100.append(lable)
+            link_top100.append(link)
+        s = {position: position_top100, title: title_top100, lable: lable_top100, link: link_top100}
+        df = pd.DataFrame(s)
+        df.columns = ['position', 'title', 'lable', 'link']
+        df['lable'] = df['lable'].str.replace("\n", "")
+        df.set_index('position')
+        return df
 
-# df_old = df_old.drop(labels=[1,2,3], axis=0)
-df_old.columns = (['Unnamed: 0', 'position_old', 'title', 'lable', 'link'])
+    t = 0
+    while request.status_code != 200:
+        time.sleep(60)
+        if t>10:
+            break
+        t += 1
+    df = fun(soup)
 
-new_releases_df = df.merge(df_old, how='left', on=['title', 'lable', 'link'], indicator=True) \
-    .query("_merge == 'left_only'") \
-    .drop(['_merge', 'Unnamed: 0', 'position_old'], axis=1)
+    bot = telegram.Bot(token=os.getenv('TOKEN'))
 
-n = 0
+    df_old = pd.read_csv('/Users/ilakozin/PycharmProjects/Beatport_top100_bot/Beatport_top100_releases.csv')
 
-for index, row in new_releases_df.iterrows():
-    t = row['title']
-    l = row['lable']
-    p = row['position']
-    li = 'https://www.beatport.com' + row['link']
-    n = n + 1
-    s = (f"Название:        {t}\nЛейбл:          {l}\nМесто в Топ100:    {p}\nСсылка: {li}\n\n\n")
-    bot.send_message(chat_id= os.getenv('CHAT_ID'), text=s)
-if n > 0:
-    df.to_csv('/Users/ilakozin/PycharmProjects/Beatport_top100_bot/Beatport_top100_releases.csv')
+    #df_old = df_old.drop(labels=[1,2,3], axis=0)
+    df_old.columns = (['Unnamed: 0', 'position_old', 'title', 'lable', 'link'])
+
+    new_releases_df = df.merge(df_old, how='left', on=['title', 'lable', 'link'], indicator=True) \
+        .query("_merge == 'left_only'") \
+        .drop(['_merge', 'Unnamed: 0', 'position_old'], axis=1)
+
+    n = 0
+
+    for index, row in new_releases_df.iterrows():
+        t = row['title']
+        l = row['lable']
+        p = row['position']
+        li = 'https://www.beatport.com' + row['link']
+        n = n + 1
+        s = (f"Название:        {t}\nЛейбл:          {l}\nМесто в Топ100:    {p}\nСсылка: {li}\n\n\n")
+        bot.send_message(chat_id= os.getenv('CHAT_ID'), text=s)
+    if n > 0:
+        df.to_csv('/Users/ilakozin/PycharmProjects/Beatport_top100_bot/Beatport_top100_releases.csv')
+logging.basicConfig(level='INFO', filename=os.path.join(dir_pass,'logdata.txt'))
+now = pd.Timestamp('now')
+logging.info(f'TEST_REPORT_START_BUILDING {now}')
+try:
+    cronos()
+    logging.info('TEST_REPORT_SENT')
+except Exception as e:
+    logging.exception(e)
