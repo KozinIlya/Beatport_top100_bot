d2bd390875bcde638c2c61fb112439da094d36df
ilakozin <i.a.kozin@gmail.com>
2022-08-15 14:03:48 +0300

    fin. file

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
+import logging
+import os
 import time
-import telegram
+import io
 import pandas as pd
 import requests
+import telegram
 from bs4 import BeautifulSoup
 import settings
 import os
 import io
 import logging
 dir_pass = '/Users/ilakozin/PycharmProjects/Beatport_top100_bot/cronos_logs'
+url = 'https://www.beatport.com/top-100-releases'
+request = requests.get(url)
+soup = BeautifulSoup(request.text, 'html.parser')
+
+dir_pass = '/Users/ilakozin/PycharmProjects/Beatport_top100_bot/cronos_logs'
+
 def cronos(chat=None):
-    url = 'https://www.beatport.com/top-100-releases'
-    request = requests.get(url)
-    soup = BeautifulSoup(request.text, 'html.parser')
-
-
     def fun(s):
+        global position, title, lable, link
         t = s.find_all('li', class_="bucket-item ec-item horz-release")
         title_top100 = []
         position_top100 = []
@@ -38,27 +43,21 @@ def cronos(chat=None):
         df.set_index('position')
         return df
 
-    t = 0
     while request.status_code != 200:
         time.sleep(60)
-        if t>10:
-            break
-        t += 1
+        print('sleep')
     df = fun(soup)
 
     bot = telegram.Bot(token=os.getenv('TOKEN'))
-
     df_old = pd.read_csv('/Users/ilakozin/PycharmProjects/Beatport_top100_bot/Beatport_top100_releases.csv')
-
-    #df_old = df_old.drop(labels=[1,2,3], axis=0)
+    df_old = df_old.drop(labels=[1,2,3], axis=0)
     df_old.columns = (['Unnamed: 0', 'position_old', 'title', 'lable', 'link'])
 
     new_releases_df = df.merge(df_old, how='left', on=['title', 'lable', 'link'], indicator=True) \
-        .query("_merge == 'left_only'") \
-        .drop(['_merge', 'Unnamed: 0', 'position_old'], axis=1)
+            .query("_merge == 'left_only'") \
+            .drop(['_merge', 'Unnamed: 0', 'position_old'], axis=1)
 
     n = 0
-
     for index, row in new_releases_df.iterrows():
         t = row['title']
         l = row['lable']
@@ -67,9 +66,10 @@ def cronos(chat=None):
         n = n + 1
         s = (f"Название:        {t}\nЛейбл:          {l}\nМесто в Топ100:    {p}\nСсылка: {li}\n\n\n")
         bot.send_message(chat_id= os.getenv('CHAT_ID'), text=s)
-    if n > 0:
-        df.to_csv('/Users/ilakozin/PycharmProjects/Beatport_top100_bot/Beatport_top100_releases.csv')
-logging.basicConfig(level='INFO', filename=os.path.join(dir_pass,'logdata.txt'))
+        if n > 0:
+            df.to_csv('/Users/ilakozin/PycharmProjects/Beatport_top100_bot/Beatport_top100_releases.csv')
+
+logging.basicConfig(level='INFO', filename=os.path.join(dir_pass, 'logdata.txt'))
 now = pd.Timestamp('now')
 logging.info(f'TEST_REPORT_START_BUILDING {now}')
 try:
